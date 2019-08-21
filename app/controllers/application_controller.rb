d651816593db341f81ffe7d9045637d6ba93ce90
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-21 19:35:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
-  end
+    @user = User.find_by(username: params[:username])
+    if @user.username && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+      erb :error
+    end
 
   get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
 
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
+
+    redirect '/'
 
   end
 
 
 end
-
